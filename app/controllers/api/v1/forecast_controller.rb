6f0a8ca9480cb9e92af18f0db8c6ccc88abc8d51
Wil McCauley <wmccauley724@gmail.com>
2021-04-23 17:11:51 -0600

    Begin connecting lat long to weather connection for forecast data payload

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ForecastController < ApplicationController
   def local_five_day
     loc = params[:location]
     lat_lng = get_coords(loc)
+    raw_weather = get_five_day(lat_lng[:lat], lat_lng[:lng])
   end
 
   def coordinate_connection
@@ -16,10 +17,14 @@ class Api::V1::ForecastController < ApplicationController
     lat_lng_hash = data[:results][0][:locations][0][:latLng]
   end
 
-  # def weather_connection
-    # weather_connection ||= Faraday.new({
-      # url:
-    # })
-  # end
+  def weather_connection
+    weather_connection ||= Faraday.new({
+      url:
+    })
+  end
+
+  def get_five_day(lat, lon)
+    res = weather_connection.get("/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude={minutely,alerts}&appid=#{ENV['WEATHER_KEY']}")
+  end
 
 end
