95f41fada9d9191d296f8e2b95e22dd3a0fcba96
Wil McCauley <wmccauley724@gmail.com>
2021-04-25 14:42:41 -0600

    Implement initial Bcrypt password handling with model validations

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,17 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    
+    new_user = User.new(user_params)
+    require "pry"; binding.pry
   end
 
 
   private
 
   def user_params
-    new_user_params = params.require(:user).permit(:email, :password, :password_confirmation)
+    new_user_params = JSON.parse(request.body.read, symbolize_names: true)
     new_user_params[:email].downcase
+    key_gen = SecureRandom.base64
+    new_user_params[:api_key] = key_gen
     new_user_params
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:api_key) }
+  end
 end
