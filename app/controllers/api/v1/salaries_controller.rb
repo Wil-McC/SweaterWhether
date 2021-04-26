b541c7da2d15749095135c121581745bc6e5642c
Wil McCauley <wmccauley724@gmail.com>
2021-04-26 09:24:17 -0600

    Begin connection for salaries request

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,3 +1,24 @@
 class Api::V1::SalariesController < ApplicationController
+  def salary_forecast
+    des = params[:destination]
 
+    # lat_lng = GeoService.get_coords(des)
+    # extended = ForecastService.get_five_day(lat_lng[:lat], lat_lng[:lng])
+    # forecast = extended.current_weather
+
+    salary_data = fetch_salaries(des)
+  end
+
+
+  def fetch_salaries(des)
+    res = ua_connection.get("/api/urban_areas/slug:#{des}/salaries")
+
+    data = JSON.parse(res.body, symbolize_names: true)
+  end
+
+  def self.ua_connection
+    ua_connection ||= Faraday.new({
+      url: 'https://api.teleport.org'
+    })
+  end
 end
